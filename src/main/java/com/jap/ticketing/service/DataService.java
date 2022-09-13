cdef8d5540445b003b5ab2b36e356f169317d666
Sarthak Agarwal <sarthakaggarwal24@gmail.com>
2022-09-13 10:33:06 +0530

    added the method sortByDistance()

--- src/main/java/com/jap/ticketing/service/DataService.java
+++ src/main/java/com/jap/ticketing/service/DataService.java
@@ -1,4 +1,11 @@
 package com.jap.ticketing.service;
 
+import com.jap.ticketing.model.Ticket;
+
+import java.util.List;
+
 public class DataService {
+    public List<Ticket> sortByDistance (List<Ticket> ticketList) {
+        return ticketList;
+    }
 }
