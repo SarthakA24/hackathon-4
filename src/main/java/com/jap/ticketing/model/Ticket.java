e77adfcecb324ed3334cebfae9d9e610da661b8c
Sarthak Agarwal <sarthakaggarwal24@gmail.com>
2022-09-13 10:12:32 +0530

    added the getter, setter, toString, hashCode and equals methods

--- src/main/java/com/jap/ticketing/model/Ticket.java
+++ src/main/java/com/jap/ticketing/model/Ticket.java
@@ -1,5 +1,7 @@
 package com.jap.ticketing.model;
 
+import java.util.Objects;
+
 public class Ticket {
     private String scheduleNumber;
     private String routeNumber;
@@ -26,5 +28,114 @@ public class Ticket {
         this.ticketTime = ticketTime;
         this.totalTicketAmount = totalTicketAmount;
         this.travelledKilometer = travelledKilometer;
+    }
+
+    public String getScheduleNumber() {
+        return scheduleNumber;
+    }
+
+    public void setScheduleNumber(String scheduleNumber) {
+        this.scheduleNumber = scheduleNumber;
+    }
+
+    public String getRouteNumber() {
+        return routeNumber;
+    }
+
+    public void setRouteNumber(String routeNumber) {
+        this.routeNumber = routeNumber;
+    }
+
+    public int getTicketFromStopId() {
+        return ticketFromStopId;
+    }
+
+    public void setTicketFromStopId(int ticketFromStopId) {
+        this.ticketFromStopId = ticketFromStopId;
+    }
+
+    public int getTicketFromStopSequenceNumber() {
+        return ticketFromStopSequenceNumber;
+    }
+
+    public void setTicketFromStopSequenceNumber(int ticketFromStopSequenceNumber) {
+        this.ticketFromStopSequenceNumber = ticketFromStopSequenceNumber;
+    }
+
+    public int getTicketTillStopId() {
+        return ticketTillStopId;
+    }
+
+    public void setTicketTillStopId(int ticketTillStopId) {
+        this.ticketTillStopId = ticketTillStopId;
+    }
+
+    public int getTicketTillStopSequenceNumber() {
+        return ticketTillStopSequenceNumber;
+    }
+
+    public void setTicketTillStopSequenceNumber(int ticketTillStopSequenceNumber) {
+        this.ticketTillStopSequenceNumber = ticketTillStopSequenceNumber;
+    }
+
+    public String getTicketDate() {
+        return ticketDate;
+    }
+
+    public void setTicketDate(String ticketDate) {
+        this.ticketDate = ticketDate;
+    }
+
+    public String getTicketTime() {
+        return ticketTime;
+    }
+
+    public void setTicketTime(String ticketTime) {
+        this.ticketTime = ticketTime;
+    }
+
+    public double getTotalTicketAmount() {
+        return totalTicketAmount;
+    }
+
+    public void setTotalTicketAmount(double totalTicketAmount) {
+        this.totalTicketAmount = totalTicketAmount;
+    }
+
+    public double getTravelledKilometer() {
+        return travelledKilometer;
+    }
+
+    public void setTravelledKilometer(double travelledKilometer) {
+        this.travelledKilometer = travelledKilometer;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Ticket)) return false;
+        Ticket ticket = (Ticket) o;
+        return getTicketFromStopId() == ticket.getTicketFromStopId() && getTicketFromStopSequenceNumber() == ticket.getTicketFromStopSequenceNumber() && getTicketTillStopId() == ticket.getTicketTillStopId() && getTicketTillStopSequenceNumber() == ticket.getTicketTillStopSequenceNumber() && Double.compare(ticket.getTotalTicketAmount(), getTotalTicketAmount()) == 0 && Double.compare(ticket.getTravelledKilometer(), getTravelledKilometer()) == 0 && Objects.equals(getScheduleNumber(), ticket.getScheduleNumber()) && Objects.equals(getRouteNumber(), ticket.getRouteNumber()) && Objects.equals(getTicketDate(), ticket.getTicketDate()) && Objects.equals(getTicketTime(), ticket.getTicketTime());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getScheduleNumber(), getRouteNumber(), getTicketFromStopId(), getTicketFromStopSequenceNumber(), getTicketTillStopId(), getTicketTillStopSequenceNumber(), getTicketDate(), getTicketTime(), getTotalTicketAmount(), getTravelledKilometer());
+    }
+
+    @Override
+    public String toString() {
+        return "Ticket{" +
+                "scheduleNumber='" + scheduleNumber + '\'' +
+                ", routeNumber='" + routeNumber + '\'' +
+                ", ticketFromStopId=" + ticketFromStopId +
+                ", ticketFromStopSequenceNumber=" + ticketFromStopSequenceNumber +
+                ", ticketTillStopId=" + ticketTillStopId +
+                ", ticketTillStopSequenceNumber=" + ticketTillStopSequenceNumber +
+                ", ticketDate='" + ticketDate + '\'' +
+                ", ticketTime='" + ticketTime + '\'' +
+                ", totalTicketAmount=" + totalTicketAmount +
+                ", travelledKilometer=" + travelledKilometer +
+                '}';
     }
 }
