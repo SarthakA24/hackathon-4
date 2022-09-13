0a3396a086737b70eb8768f3a7894c5da1a72e12
Sarthak Agarwal <sarthakaggarwal24@gmail.com>
2022-09-13 10:39:28 +0530

    added JavaDocs for the method sortByDistance()

--- src/main/java/com/jap/ticketing/service/DataService.java
+++ src/main/java/com/jap/ticketing/service/DataService.java
@@ -6,6 +6,12 @@ import java.util.Comparator;
 import java.util.List;
 
 public class DataService {
+
+    /**
+     * This method sorts the given Tickets List in descending order by Distance Travelled
+     * @param ticketList The Ticket List that needs to be sorted
+     * @return The sorted Ticket List
+     */
     public List<Ticket> sortByDistance (List<Ticket> ticketList) {
         ticketList.sort(Comparator.comparingDouble(Ticket::getTravelledKilometer).reversed());
         return ticketList;
