9f6fbfc071d88fa6a2df21f4823c5ea5e69d50d4
Sarthak Agarwal <sarthakaggarwal24@gmail.com>
2022-09-13 10:11:18 +0530

    added the constructors for ticket class

--- src/main/java/com/jap/ticketing/model/Ticket.java
+++ src/main/java/com/jap/ticketing/model/Ticket.java
@@ -11,4 +11,20 @@ public class Ticket {
     private String ticketTime;
     private double totalTicketAmount;
     private double travelledKilometer;
+
+    public Ticket() {
+    }
+
+    public Ticket(String scheduleNumber, String routeNumber, int ticketFromStopId, int ticketFromStopSequenceNumber, int ticketTillStopId, int ticketTillStopSequenceNumber, String ticketDate, String ticketTime, double totalTicketAmount, double travelledKilometer) {
+        this.scheduleNumber = scheduleNumber;
+        this.routeNumber = routeNumber;
+        this.ticketFromStopId = ticketFromStopId;
+        this.ticketFromStopSequenceNumber = ticketFromStopSequenceNumber;
+        this.ticketTillStopId = ticketTillStopId;
+        this.ticketTillStopSequenceNumber = ticketTillStopSequenceNumber;
+        this.ticketDate = ticketDate;
+        this.ticketTime = ticketTime;
+        this.totalTicketAmount = totalTicketAmount;
+        this.travelledKilometer = travelledKilometer;
+    }
 }
