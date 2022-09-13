bc947de7b14e4d73eab430517c756f839b9b0b12
Sarthak Agarwal <sarthakaggarwal24@gmail.com>
2022-09-13 10:18:24 +0530

    created an instance for BufferedReader in a try catch block

--- src/main/java/com/jap/ticketing/service/FileReaderService.java
+++ src/main/java/com/jap/ticketing/service/FileReaderService.java
@@ -2,6 +2,10 @@ package com.jap.ticketing.service;
 
 import com.jap.ticketing.model.Ticket;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -9,6 +13,11 @@ public class FileReaderService {
 
     public List<Ticket> readFile (String fileName) {
         List<Ticket> ticketList = new ArrayList<>();
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName))) {
+
+        } catch (IOException exception) {
+            System.err.println(exception.getMessage());
+        }
         return ticketList;
     }
 }
