1d5e7f1f855a946178622ea88bb77d13c4908433
Sarthak Agarwal <sarthakaggarwal24@gmail.com>
2022-09-13 10:25:25 +0530

    read the lines from the CSV file and stored the values of each line in the list

--- src/main/java/com/jap/ticketing/service/FileReaderService.java
+++ src/main/java/com/jap/ticketing/service/FileReaderService.java
@@ -3,7 +3,6 @@ package com.jap.ticketing.service;
 import com.jap.ticketing.model.Ticket;
 
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -14,7 +13,33 @@ public class FileReaderService {
     public List<Ticket> readFile (String fileName) {
         List<Ticket> ticketList = new ArrayList<>();
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName))) {
-
+            String readLine = bufferedReader.readLine();
+            while ((readLine = bufferedReader.readLine()) != null) {
+                String[] data = readLine.split(",");
+                String scheduleNumber = data[0].trim();
+                String routeNumber = data[1].trim();
+                int ticketFromStopId = Integer.parseInt(data[2]);
+                int ticketFromStopSequenceNumber = Integer.parseInt(data[3]);
+                int ticketTillStopId = Integer.parseInt(data[4]);
+                int ticketTillStopSequenceNumber = Integer.parseInt(data[5]);
+                String ticketDate = data[6].trim();
+                String ticketTime = data[7].trim();
+                double totalTicketAmount = Double.parseDouble(data[8]);
+                double travelledKilometer = Double.parseDouble(data[9]);
+                Ticket ticket = new Ticket(
+                        scheduleNumber,
+                        routeNumber,
+                        ticketFromStopId,
+                        ticketFromStopSequenceNumber,
+                        ticketTillStopId,
+                        ticketTillStopSequenceNumber,
+                        ticketDate,
+                        ticketTime,
+                        totalTicketAmount,
+                        travelledKilometer
+                );
+                ticketList.add(ticket);
+            }
         } catch (IOException exception) {
             System.err.println(exception.getMessage());
         }
