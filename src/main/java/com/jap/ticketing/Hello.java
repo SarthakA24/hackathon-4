7fb8faaf718901810232799155d6027b017b1d74
Sarthak Agarwal <sarthakaggarwal24@gmail.com>
2022-09-13 10:46:59 +0530

    read the data from the CSV file in the main method and saved the values in a List of tickets

--- src/main/java/com/jap/ticketing/Hello.java
+++ src/main/java/com/jap/ticketing/Hello.java
@@ -1,8 +1,15 @@
 package com.jap.ticketing;
 
+import com.jap.ticketing.model.Ticket;
+import com.jap.ticketing.service.FileReaderService;
+
+import java.util.List;
+
 public class Hello {
 
     public static void main(String[] args) {
-        System.out.println("Hello");
+        String fileName = "src/main/resources/sample.csv";
+        FileReaderService readerService = new FileReaderService();
+        List<Ticket> ticketList = readerService.readFile(fileName);
     }
 }
