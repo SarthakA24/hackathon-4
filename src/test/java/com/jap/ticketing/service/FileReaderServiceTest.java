8bcc16327ea0b56e7750a81f1ae2f4d31f04da52
Sarthak Agarwal <sarthakaggarwal24@gmail.com>
2022-09-13 11:18:32 +0530

    wrote the test for readFileSuccess()

--- src/test/java/com/jap/ticketing/service/FileReaderServiceTest.java
+++ src/test/java/com/jap/ticketing/service/FileReaderServiceTest.java
@@ -1,12 +1,17 @@
 package com.jap.ticketing.service;
 
+import com.jap.ticketing.model.Ticket;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 class FileReaderServiceTest {
     FileReaderService fileReaderService;
     private final String FILENAME = "src/test/resources/sample.csv";
+    private final String INCORRECT_FILENAME = "";
 
     @BeforeEach
     void setUp() {
@@ -20,5 +25,19 @@ class FileReaderServiceTest {
 
     @Test
     void readFileSuccess() {
+        List<Ticket> ticketList = fileReaderService.readFile(FILENAME);
+        Ticket ticket = new Ticket(
+                "KIAS-12/5",
+                "KIAS-12UP",
+                9387,
+                1,
+                11359,
+                39,
+                "01/09/2018",
+                "02:02:58",
+                281.0,
+                49.3
+        );
+        Assertions.assertEquals(ticket, ticketList.get(0), "File has been Read Successfully!");
     }
 }
