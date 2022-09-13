660e624fc7c2e7fd9e3ca3caeb3a2ae846b5eaa6
Sarthak Agarwal <sarthakaggarwal24@gmail.com>
2022-09-13 10:10:52 +0530

    added the fields for the Ticket class

--- src/main/java/com/jap/ticketing/model/Ticket.java
+++ src/main/java/com/jap/ticketing/model/Ticket.java
@@ -1,4 +1,14 @@
 package com.jap.ticketing.model;
 
 public class Ticket {
+    private String scheduleNumber;
+    private String routeNumber;
+    private int ticketFromStopId;
+    private int ticketFromStopSequenceNumber;
+    private int ticketTillStopId;
+    private int ticketTillStopSequenceNumber;
+    private String ticketDate;
+    private String ticketTime;
+    private double totalTicketAmount;
+    private double travelledKilometer;
 }
