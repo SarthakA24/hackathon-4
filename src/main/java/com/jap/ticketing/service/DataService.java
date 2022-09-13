d7bfc08ce827fb09ea259fd9ed852cb909805062
Sarthak Agarwal <sarthakaggarwal24@gmail.com>
2022-09-13 10:34:40 +0530

    created an anonymous class for Comparator and wrote the logic for comparing

--- src/main/java/com/jap/ticketing/service/DataService.java
+++ src/main/java/com/jap/ticketing/service/DataService.java
@@ -2,10 +2,17 @@ package com.jap.ticketing.service;
 
 import com.jap.ticketing.model.Ticket;
 
+import java.util.Comparator;
 import java.util.List;
 
 public class DataService {
     public List<Ticket> sortByDistance (List<Ticket> ticketList) {
+        ticketList.sort(new Comparator<Ticket>() {
+            @Override
+            public int compare(Ticket o1, Ticket o2) {
+                return Double.compare(o2.getTravelledKilometer(),o1.getTravelledKilometer());
+            }
+        });
         return ticketList;
     }
 }
