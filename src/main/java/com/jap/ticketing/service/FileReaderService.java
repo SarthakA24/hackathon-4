eb2042f40cb19c917ec307f19f7a6bd6144caf80
Sarthak Agarwal <sarthakaggarwal24@gmail.com>
2022-09-13 10:16:52 +0530

    added the method readFile() in the class FileReaderService.java

--- src/main/java/com/jap/ticketing/service/FileReaderService.java
+++ src/main/java/com/jap/ticketing/service/FileReaderService.java
@@ -1,4 +1,14 @@
 package com.jap.ticketing.service;
 
+import com.jap.ticketing.model.Ticket;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class FileReaderService {
+
+    public List<Ticket> readFile (String fileName) {
+        List<Ticket> ticketList = new ArrayList<>();
+        return ticketList;
+    }
 }
